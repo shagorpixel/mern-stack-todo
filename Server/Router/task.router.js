0f1eca8e5e415ae287ba0c2c68b29e118273635f
shagorpixel <shagorpixel@gmail.com>
2025-05-08 22:28:31 +0600

    Complete Backend Rest Api

--- Server/Router/task.router.js
+++ Server/Router/task.router.js
@@ -1,7 +1,11 @@
-const { createTask } = require('../Controller/taskController');
+const { createTask, getTask, upDateTask, singleTask, deleteTask } = require('../Controller/taskController');
 
 const taskRouter = require('express').Router();
 
-taskRouter.post('/',createTask)
+taskRouter.post('/',createTask);
+taskRouter.get('/',getTask);
+taskRouter.get('/:id',singleTask);
+taskRouter.put('/:id',upDateTask);
+taskRouter.delete('/:id',deleteTask);
 
 module.exports = taskRouter
